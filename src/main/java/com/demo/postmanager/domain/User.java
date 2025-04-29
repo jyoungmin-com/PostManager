295c539f43097a4a6f9831a799a18f3700a6d9e0
Youngmin Jung <youngmin.jung@outlook.com>
2025-04-29 15:06:17 +0900

    Initial commit

---
+++ src/main/java/com/demo/postmanager/domain/User.java
@@ -0,0 +1,7 @@
+package com.demo.postmanager.domain.constant;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+  }
---
+++ src/main/java/com/demo/postmanager/domain/constant/UserRoleType.java
@@ -0,0 +1,4 @@
+package com.demo.postmanager.domain.constant;
+
+public enum UserRoleType {
+}
